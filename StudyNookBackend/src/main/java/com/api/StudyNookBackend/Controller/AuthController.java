a253f0fa4cca378e9610cc6734e278e69ade9e55
Jessica <diackjessica5@gmail.com>
2025-08-10 15:24:48 -0400

    feat: add create user and user signup controller logic

--- StudyNookBackend/src/main/java/com/api/StudyNookBackend/Controller/AuthController.java
+++ StudyNookBackend/src/main/java/com/api/StudyNookBackend/Controller/AuthController.java
@@ -1,10 +1,47 @@
 package com.api.StudyNookBackend.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.StudyNookBackend.DTO.UserDTO;
+import com.api.StudyNookBackend.Entity.User;
+import com.api.StudyNookBackend.Repository.UserRepository;
+import com.api.StudyNookBackend.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 //handles endpoint logic for auth
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
+    private final Pattern pattern = Pattern.compile("^ [A-Za-z0-9+_.-]+@[A_Za-z0-9.-]+$");
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> createNewUser(@RequestBody UserDTO dto){
+        Matcher matcher = pattern.matcher(dto.getEmail());
+        if(!matcher.matches()){
+            throw new RuntimeException("invalid email");
+        }
+        if(dto.getPassword().length() < 6){
+            throw new RuntimeException("password length needs to be minimum 6 characters");
+        }
+
+        User user = userService.createNewUser(dto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(user);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("invalid input: " + ex.getMessage());
+    }
 }
--- StudyNookBackend/src/main/java/com/api/StudyNookBackend/Service/UserService.java
+++ StudyNookBackend/src/main/java/com/api/StudyNookBackend/Service/UserService.java
@@ -1,8 +1,39 @@
 package com.api.StudyNookBackend.Service;
 
+import com.api.StudyNookBackend.DTO.UserDTO;
+import com.api.StudyNookBackend.Entity.User;
+import com.api.StudyNookBackend.Repository.UserRepository;
+import com.api.StudyNookBackend.Util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 //handles all the functionality for the user
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private PasswordEncoder encoder;
+
+    public UserService(){}
+
+    //create a new user
+    public User createNewUser(UserDTO userdto){
+        User existingUser = userRepository.findByEmail(userdto.getEmail()).orElseThrow(IllegalArgumentException::new);
+        if(userRepository.count() >= 1 && existingUser != null){
+            throw new RuntimeException("user already exists");
+        }
+        User user = new User();
+        user.setEmail(userdto.getEmail());
+        user.setPassword(encoder.encode(userdto.getPassword()));
+        user.setName(userdto.getName());
+
+        return userRepository.save(user);
+    }
+
 }
